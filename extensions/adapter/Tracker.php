6b4d1904d5659def47a95c7dd91dd36556a29214
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-09-12 15:09:50 -0500

    Have a _views array instead of _section string.

--- extensions/adapter/Tracker.php
+++ extensions/adapter/Tracker.php
@@ -6,6 +6,8 @@ abstract class Tracker extends \lithium\core\Object {
 	
 	protected $_name;
 
+	protected $_views = array();
+
 	abstract protected function key();
 
 	public function __construct($config){
