15924f383a6030570c0a06f67bfd09f376742dce
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-09-18 10:11:35 -0500

    Add new unit tests to check config of the views

--- tests/cases/extensions/adapter/tracking/GoogleAnalyticsTest.php
+++ tests/cases/extensions/adapter/tracking/GoogleAnalyticsTest.php
@@ -49,4 +49,73 @@ class GoogleAnalyticsTest extends \lithium\test\Unit
 
 		$this->assertEqual($expected, $ga->commands());
 	}
+
+	function test_view_position()
+	{
+		$ga = new GoogleAnalytics(array(
+			'adapter' => 'GoogleAnalytics',
+			'account' => 'abc123',
+			'section' => 'prepend_head',
+			'commands' => array(
+				array('_hello'),
+				array('a', 'b', 'c', 'd'),
+			)
+		));
+		$views = $ga->views();
+		$keys = array_keys($views);
+		$this->assertEqual('prepend_head', $keys[0]);
+	}
+
+	function test_view_element()
+	{
+		$ga = new GoogleAnalytics(array(
+			'adapter' => 'GoogleAnalytics',
+			'account' => 'abc123',
+			'element' => 'google_2',
+			'commands' => array(
+				array('_hello'),
+				array('a', 'b', 'c', 'd'),
+			)
+		));
+		$views = $ga->views();
+		$this->assertEqual('google_2', array_pop($views));
+	}
+
+	function test_view_position_element()
+	{
+		$ga = new GoogleAnalytics(array(
+			'adapter' => 'GoogleAnalytics',
+			'account' => 'abc123',
+			'section' => 'append_head',
+			'element' => 'google_2',
+			'commands' => array(
+				array('_hello'),
+				array('a', 'b', 'c', 'd'),
+			)
+		));
+		$views = $ga->views();
+		$this->assertIdentical(1, count($views)); // Length
+		$this->assertEqual(array('append_head'=>'google_2'), $views); // key/value
+	}
+
+	function test_view_multi_views()
+	{
+		$ga = new GoogleAnalytics(array(
+			'adapter' => 'GoogleAnalytics',
+			'account' => 'abc123',
+			'views' => array(
+				'prepend_head' => 'google_2',
+				'prepend_body' => 'google_3'
+			),
+			'commands' => array(
+				array('_hello'),
+				array('a', 'b', 'c', 'd'),
+			)
+		));
+		$views = $ga->views();
+		$this->assertEqual(array(
+			'prepend_head' => 'google_2',
+			'prepend_body' => 'google_3'
+		), $views); // key/value
+	}
 }
