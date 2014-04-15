7a9cfabd4c02647808738dc883cb8eec4907882d
weiWork <vagrant@localhost.localdomain>
2014-04-15 14:13:00 +0000

    vit-2741::added google tag manager support to li3_analytics
    vit-2741::refined format
    
    vit-2741::changed file name
    
    vit-2741::removed the noscript check since gtm always turn it on
    
    vit-2741::removed noscript and session

---
+++ extensions/adapter/tracking/GoogleTagManager.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace li3_analytics\extensions\adapter\tracking;
+
+class GoogleTagManager extends \li3_analytics\extensions\adapter\Tracker {
+	/**
+	 * Google Tag Manager account
+	 *
+	 * @var string
+	 */
+	protected $_account;
+
+	/**
+	 * What section of the page should the script be loaded?
+	 * @var array
+	 */
+	protected $_views = array(
+		'prepend_body' => 'googletagmanager'
+	);
+
+	/**
+	 * Way to load tracker
+	 * `block`, `inline`
+	 * `block` loads a javascript block
+	 * `inline` loads a javascript link.
+	 * @var string
+	 */
+	protected $_type = "block";
+
+	protected $_autoConfig = array('account', 'views');
+
+	/**
+	 * Tracking account used
+	 *
+	 * @return string tracking account
+	 */
+	public function account() {
+		return trim($this->_account);
+	}
+
+	/**
+	 * Unified method to get account details
+	 * @return string
+	 */
+	public function key(){
+		return $this->account();
+	}
+
+	public function configuration(){
+		return !empty($this->_configuration) ? $this->_configuration : false;
+	}
+}
